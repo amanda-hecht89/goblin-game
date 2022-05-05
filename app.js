9ed47b37b7e6e7d5e652fe1f2122d8c758a8b76b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-05 16:43:30 -0600

    start app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // import functions and grab DOM elements
-
+const defeatedNumEl = document.querySelector('#defeated-number');
+const playerHpEl = document.querySelector('#player-hp');
+const playerImgEl = document.querySelector('#player-img');
+const form = document.querySelector('form');
+const elfListEl = document.querySelector('.elves');
 // let state
+let defeatedElfCount = 0;
+let playerHp = 2;
+let elves = [
+    { name: 'Jingles', hp: 1 },
+    { name: 'Captain HappyDance', hp: 5 },
+];
 
 // set event listeners 
   // get user input
